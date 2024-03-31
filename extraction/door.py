180fe98a4f95c0938c1dcf5bd51acc6487c740bd
Huda Baig <hudab@andrew.cmu.edu>
2024-03-31 19:04:09 +0300

    moved door_boxes function

--- extraction/door.py
+++ extraction/door.py
@@ -1,4 +1,9 @@
 from utils.graph import *
+from .door_model.detect import detection_model, img_transform, inference
+
+import pandas as pd
+import os
+root = os.path.dirname(__file__) + "/../"
 
 """
 creates door nodes based on given door bounding boxes and adds them to 
@@ -22,3 +27,29 @@ def make_doors(door_df, G):
         G.add_node(node(id=G.next_id, area="Door", coord=center, type="door"))
 
     return G
+
+"""
+performs inference on image at given location and returns a dataframe of all found doors
+
+parameters
+- image_path: location of image to infer on, str
+    (required)
+- weights_pth: location to pretrained model weights, str
+    (default '../model_weights/door_mdl_32.pth')
+- thresh: threshold of confidence to consider inference valid, float
+    (default 0.0)
+
+returns 
+    dataframe of door bounding boxes with columns 
+    'xmin' 'ymin' 'xmax' 'ymax', pd.DataFrame
+"""
+def door_boxes(image_path, weights_pth=root + "model_weights/door_mdl_32.pth", 
+               thresh=0):
+    model = detection_model(weights_pth)
+
+    # transform and infer
+    img = img_transform(cv2.imread(image_path))
+    boxes, scores, labels = inference(img, model, detection_threshold=thresh)
+
+    # make dataframe from found boxes
+    return pd.DataFrame(boxes, columns=["xmin","ymin","xmax","ymax"])
