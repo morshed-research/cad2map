afdf4cec248ba048133e4baba21399f21447a16a
Huda Baig <hudab@andrew.cmu.edu>
2024-04-05 15:37:27 +0300

    added model weights download

---
+++ install/weights.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gdown --fuzzy "https://drive.google.com/file/d/1T4U0AphgbGEggWETNY2ATqfUC8CGIe4p/view?usp=sharing" -O model_weights/
+gdown --fuzzy "https://drive.google.com/file/d/1hzna8NWNY1k7Jt-0BHe1Lg8wXCVNqJ-2/view?usp=sharing" -O model_weights/
+gdown --fuzzy "https://drive.google.com/file/d/1HBDhdmhLNmCH0sAk52v3w3AqiZ6k4QnI/view?usp=sharing" -O model_weights/
