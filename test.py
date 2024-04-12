af025cd98e66b8a5813ef815fe9f096bce72afe9
Huda Baig <hudab@andrew.cmu.edu>
2024-04-12 22:52:34 +0300

    testing path similarity

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
 from eval.testbed import to_graph
+from eval.metrics import Path_Similarity
 
-file = open("data/ground-truth/jsons/West-Walkway-graph.json", "r")
-G = to_graph(file, 0.42)
+file = open("data/ground-truth/jsons/test_floorplan.json", "r")
+target = to_graph(file)
+file.close()
 
-G.draw("data/ground-truth/raw/West-Walkway.png", "results/west-walkway-testbed.png")
+file = open("results/json/test-floorplan-labelled-graph.json", "r")
+source = to_graph(file)
+file.close()
+
+target.draw("data/test-floorplan-labelled.png", "results/basic.png")
+source.draw("data/test-floorplan-labelled.png", "results/basi-pipe.png")
+
+metric = Path_Similarity(source, target)
+print(metric.evaluate())
+
+# G.draw("data/ground-truth/raw/West-Walkway.png", "results/west-walkway-testbed.png")
