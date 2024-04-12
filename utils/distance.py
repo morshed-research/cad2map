6083f580e3b969b502515718dfd2f40ee898c602
Huda Baig <hudab@andrew.cmu.edu>
2024-04-12 23:46:14 +0300

    eval util functions

--- utils/distance.py
+++ utils/distance.py
@@ -49,3 +49,77 @@ def node_dist(n1, n2):
     center = ((x1 + x2) // 2, (y1 + y2) // 2)
 
     return dist, center
+
+"""
+returns the euclidean distance between two nodes to use as the
+edge weight between them
+
+parameters:
+- n1: first node on edge, node
+    (required)
+- n2: second node on edge, node
+    (required)
+- edge: edge properties, dict
+    (required, unused)
+
+returns
+    euclidean distance, int
+"""
+def euclidean_weight(n1, n2, edge):
+    (dist, centre) = node_dist(n1, n2)
+    return dist
+
+"""
+returns true if nodes are within 1 metre of each other, false otherwise 
+
+parameters:
+- n1: first node to check, node
+    (required)
+- n2: second node to compare to, node
+    (required)
+
+returns
+    bool
+"""
+def coord_match(n1, n2):
+    (x1, y1) = n1.coordinates
+    (x2, y2) = n2.coordinates
+    one_metre = dist2pixel(1)
+
+    return (abs(x1 - x2) <= one_metre) and (abs(y1 - y2) <= one_metre) 
+
+"""
+finds first node in paths that is within 1 metre of node n
+
+parameters:
+- n: node to find, node
+    (required)
+- paths: nodes to search amongst, list or dict
+    (required)
+
+returns
+    node close to n, node
+"""
+def find(n, paths):
+    for n2 in paths:
+        if coord_match(n, n2):
+            return n2 
+    
+    return None
+
+"""
+returns node with label entrance, else last node if none exists
+
+parameters:
+- G: graph to search, graph
+    (required)
+
+returns 
+    node
+"""
+def find_entrance(G):
+    for n in G.nx_graph:
+        if n.area_label.lower() == "entrance":
+            return n 
+    
+    return n # last node found
