86ffaf556146cd1647076d2782492240758decad
Huda Baig <hudab@andrew.cmu.edu>
2024-04-24 18:38:48 +0300

    upscaled majlis

--- test.py
+++ test.py
@@ -3,19 +3,20 @@ from eval.metrics import Path_Similarity
 
 import networkx as nx
 
-file = open("data/ground-truth/jsons/Student-Affairs-graph-20.json", "r")
-target = to_graph(file)
+file = open("data/ground-truth/jsons/Majlis-graph.json", "r")
+target = to_graph(file, scale=0.5)
 file.close()
+target.to_json("data/ground-truth/jsons/Majlis-50-graph.json")
 
-file = open("results/json/ground-truth-raw-Student-Affairs-labelled-20-graph.json", "r")
-source = to_graph(file)
-file.close()
+# file = open("results/json/ground-truth-raw-Majlis-labelled-50-graph.json", "r")
+# source = to_graph(file)
+# file.close()
 
-target.draw("data/ground-truth/raw/Student-Affairs-labelled.png", "results/basic.png")
-source.draw("data/ground-truth/raw/Student-Affairs-labelled.png", "results/basic-pipe.png")
+target.draw("data/ground-truth/raw/Majlis-labelled-50.png", "results/basic.png")
+# source.draw("data/ground-truth/raw/Student-Affairs-labelled.png", "results/basic-pipe.png")
 
-metric = Path_Similarity(source, target)
-print(metric.evaluate())
+# metric = Path_Similarity(source, target)
+# print(metric.evaluate())
 
 # nx1, nx2 = to_IL_graph(target, source)
 # print(len(nx1.nodes), len(nx2.nodes))
