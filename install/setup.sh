b5f41d8d6419b46eaa10eb1076e3ece14bfa48ba
Huda Baig <hudab@andrew.cmu.edu>
2024-06-07 17:34:21 +0300

    added GMatch4py installation

--- install/setup.sh
+++ install/setup.sh
@@ -3,4 +3,10 @@ pip install -q torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url ht
 echo "PyTorch installed!"
 
 pip install -q -r "install/requirements.txt"
-echo "All Libraries Installed!"
+
+git clone --quiet https://github.com/Jacobe2169/GMatch4py.git
+cd GMatch4py
+pip install -q .
+cd ..
+
+echo "All Libraries installed!"
